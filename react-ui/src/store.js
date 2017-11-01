265945be873e1eabfec443a15fc33a051534420c
xantrix <x4ntrix@gmail.com>
2017-11-01 19:54:23 +0100

    typo in promiseMiddleware

--- react-ui/src/store.js
+++ react-ui/src/store.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import { createLogger as logger } from 'redux-logger';
 import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
+import promiseMiddleware from 'redux-promise-middleware';
 
 import reducer from './reducer';
 
-const middlewares = [ promise(), thunk ];
+const middlewares = [ promiseMiddleware(), thunk ];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 if (process.env.NODE_ENV !== 'production') {
